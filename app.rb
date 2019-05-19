f1ccc5c1ea99d6189d2ecb45c24de9a3daf9d389
Reno Sorensen <renojames19@gmail.com>
2019-05-19 23:13:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -14,7 +14,11 @@ module FormsLab
 
 
     post "/pirates" do
+      @pirate = Pirate.new(params["pirate"])
+      @ship1 = Ship.new(params["pirate"]["ships"][0])
+      @ship2 = Ship.new(params["pirate"]["ships"][1])
 
+      erb :'pirates/show'
     end
 
   end
